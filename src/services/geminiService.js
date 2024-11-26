ad9df506093f5d5e55b59561807338df95e81b34
Winissius Machado <104407345+winissius@users.noreply.github.com>
2024-11-26 16:48:54 -0300

    improvements gemini

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,24 +1,22 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function generateDescription(imageBuffer) {
-    const prompt =
-        "Gere uma descrição em inglês para a seguinte imagem";
+    const prompt = "Generate an English description for the following image";
 
     try {
         const image = {
-        inlineData: {
-            data: imageBuffer.toString("base64"),
-            mimeType: "image/png",
-        },
-    };
-    const res = await model.generateContent([prompt, image]);
-    return res.response.text() || "Alt-text não disponível.";
-    } catch (erro) {
-    console.error("Erro ao obter alt-text:", erro.message, erro);
-    throw new Error("Erro ao obter o alt-text do Gemini.");
+            inlineData: {
+                data: imageBuffer.toString("base64"),
+                mimeType: "image/png",
+            },
+        };
+        const res = await model.generateContent([prompt, image]);
+        return res.response.text() || "Alt-text not available.";
+    } catch (error) {
+        console.error("Error generating alt-text:", error.message, error);
+        throw new Error("Error generating alt-text with Gemini.");
     }
 }
